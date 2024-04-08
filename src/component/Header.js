da9bc8c3abd856a38d081478274d58d49c63a8c3
unknown <ajibadeifedolapo17@gmail.com>
2024-04-08 13:47:49 +0100

    Push

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,10 +1,10 @@
 import {ConnectButton} from '@web3uikit/web3';
-import bits from "../logo/bits.png"
+// import bits from "../logo/bits.png"
 
 export default function Header() {
     return (
         <div className="flex flex-row bg-[#07091a] text-white justify-between px-10 py-2">
-            <img src={bits} alt="bits" />
+            {/* <img src={bits} alt="bits" /> */}
             
             <div className="ml-auto py-2 px-4">
                 <ConnectButton moralisAuth={false} />
